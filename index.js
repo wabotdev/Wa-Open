3d54ed9dc1b0fba72551f715492eddfabb51e3c5
wabotdev <120768734+wabotdev@users.noreply.github.com>
2022-12-23 11:26:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("http").createServer((_, res) => res.end("Berjalan coy")).listen(8080)
 
 const sessionName = 'yusril'
 const donet = 'https://saweria.co/sansekai'
-const owner = ['6287878817169']
+const owner = ['6288220154946']
 const { default: sansekaiConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, getContentType } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
